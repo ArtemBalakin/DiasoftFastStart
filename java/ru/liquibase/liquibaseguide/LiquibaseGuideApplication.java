7f1f0b0d3902fefb24ecc6f96b636642fdd2f2c6
ArtemBalakin <83587958+ArtemBalakin@users.noreply.github.com>
2022-02-16 23:32:46 +0300

    Add files via upload

---
+++ java/ru/liquibase/liquibaseguide/LiquibaseGuideApplication.java
@@ -0,0 +1,13 @@
+package ru.liquibase.liquibaseguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseGuideApplication.class, args);
+	}
+
+}
